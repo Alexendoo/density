12ce911e405b8c6951784ab34dddd36c4fd6d36b
Alex Macleod <alex@macleod.io>
2017-12-23 22:02:29 +0000

    output size first followed by a tab

--- src/main.rs
+++ src/main.rs
@@ -6,37 +6,17 @@ use std::string::String;
 use std::vec::Vec;
 
 fn main() {
-    let args = env::args().skip(1);
     let vec = &mut Vec::new();
 
-    let mut max_len = 0;
-    for arg in args {
-        let len = arg.len();
-        if max_len < len {
-            max_len = len
-        }
-
-        let res = visit(&arg, vec);
-
-        output_result(res, arg);
+    for arg in env::args().skip(1) {
+        output_result(visit(&arg, vec), arg);
     }
 }
 
-fn output_result(result: io::Result<&Vec<u64>>, directory: String) {
-    match result {
-        Ok(res) => println!("{} - {} bytes/file", directory, mean(res)),
-        Err(err) => println!("{} - {}", directory, err),
-    }
-}
-
-fn mean(sizes: &Vec<u64>) -> u64 {
-    let len = sizes.len() as u64;
-    let sum: u64 = sizes.iter().sum();
-
-    sum / len
-}
-
-fn visit<'a, P: AsRef<Path>>(parent: &P, sizes: &'a mut Vec<u64>) -> io::Result<&'a Vec<u64>> {
+fn visit<'a, P: AsRef<Path>>(
+    parent: &P,
+    sizes: &'a mut Vec<u64>,
+) -> io::Result<&'a Vec<u64>> {
     let metadata = fs::metadata(parent)?;
 
     if metadata.is_dir() {
@@ -50,4 +30,18 @@ fn visit<'a, P: AsRef<Path>>(parent: &P, sizes: &'a mut Vec<u64>) -> io::Result<
     }
 
     Ok(sizes)
+}
+
+fn output_result(result: io::Result<&Vec<u64>>, directory: String) {
+    match result {
+        Ok(res) => println!("{}\t{}", mean(res), directory),
+        Err(err) => eprintln!("{}: {}", directory, err),
+    }
+}
+
+fn mean(sizes: &Vec<u64>) -> u64 {
+    let len = sizes.len() as u64;
+    let sum: u64 = sizes.iter().sum();
+
+    sum / len
 }
